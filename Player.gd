c3e5caa56c4b8dedfa4eec2de370ad7b8e9a69fb
Kinsley_ <29991933+DeerCrossing@users.noreply.github.com>
2021-07-11 00:33:09 +0800

    BUILDING THE HOOSE
    the house is made, morton can now run, morton can no longer jump and stairs now take into account that each pair needs its own group

--- Player.gd
+++ Player.gd
@@ -4,32 +4,30 @@ onready var sprite = $Sprite
 
 onready var animplayer = $AnimationPlayer
 
+var sprinting = false;
+
 func _physics_process(delta):
 	var x_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	
+	sprinting = Input.is_action_pressed("sprint");
+	
 	if x_input != 0:
-		animplayer.play("Run")
-		motion.x += x_input * ACCELERATION * delta * TARGET_FPS
-		motion.x = clamp(motion.x, -MAX_SPEED, MAX_SPEED)
 		sprite.flip_h = x_input < 0
+		if sprinting:
+			animplayer.play("Run")
+			motion.x += x_input * ACCELERATION * delta * TARGET_FPS
+			motion.x = clamp(motion.x, -MAX_SPEED, MAX_SPEED)
+		else:
+			animplayer.play("Walk")
+			motion.x += x_input * (ACCELERATION/2) * delta * TARGET_FPS
+			motion.x = clamp(motion.x, -MAX_SPEED/2, MAX_SPEED/2);
 	else:
-		animplayer.play("Stand")
+		animplayer.play("Stand");
 	
 	motion.y += GRAVITY * delta * TARGET_FPS
 	
 	if is_on_floor():
 		if x_input == 0:
 			motion.x = lerp(motion.x, 0, FRICTION * delta)
-			
-		if Input.is_action_just_pressed("ui_up"):
-			motion.y = -JUMP_FORCE
-	else:
-		animplayer.play("Jump")
-		
-		if Input.is_action_just_released("ui_up") and motion.y < -JUMP_FORCE / 2:
-			motion.y = -JUMP_FORCE / 2
-		
-		if x_input == 0:
-			motion.x = lerp(motion.x, 0, AIR_RESISTANCE * delta)
 	
 	motion = move_and_slide(motion, Vector2.UP)
--- Portal.gd
+++ Portal.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
+export var group_name :String = "";
+
 func _ready():
 	$Interactible.connect("on_interact", self, "MoveToOther")
 
 func MoveToOther(area: Area2D):
-	for i in get_tree().get_nodes_in_group("Tunnel"):
+	for i in get_tree().get_nodes_in_group(group_name):
 		if i.get_path() != self.get_path():
 			area.get_parent().global_position = i.global_position
